5a4a82f0f569749061756a25e2c8b9ff58f7bd4e
Marcelo Silva <marcelosilvaa92@gmail.com>
2023-10-27 13:53:48 +1100

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/graphql');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://marcelosilvaa92:sanchez_6@cluster0.3nzadve.mongodb.net/your-actual-database-name');
+
 
 module.exports = mongoose.connection;
